268d9da197c418beaf68120dac98bdc2b72eaa74
Vadim Tsushko <vadimtsushko@gmail.com>
2017-12-12 15:25:18 +0500

    loadTableColumnar added

--- lib/src/connector/connector.dart
+++ lib/src/connector/connector.dart
@@ -68,6 +68,33 @@ class MapdConnector {
     checkConnection();
     return client.get_version();
   }
+//  loadTable(String tableName, List<TStringRow> rows) async {
+//    checkConnection();
+//    client.load_table(session, tableName, rows);
+//    TColumn
+//    client.load_table_binary_columnar(session, table_name, cols)
+//    TRow;
+//    client.load_table_binary(session, table_name, rows)
+//  }
+
+  Future loadTableColumnar(String tableName, List<TColumn> columns,
+      {logTiming: false}) async {
+    checkConnection();
+    Stopwatch stopwatch;
+    if (logTiming) {
+      stopwatch = new Stopwatch();
+      stopwatch.start();
+    }
+    await client.load_table_binary_columnar(session, tableName, columns);
+    if (logTiming) {
+      stopwatch.stop();
+      var duration2 = stopwatch.elapsed;
+      int count = columns.first.nulls.length;
+      print(
+          '-------------------------\n $count rows inserted. Browser execution time: ${duration2.inMilliseconds}');
+      stopwatch.reset();
+    }
+  }
 
   Future<TServerStatus> getServerStatus() async {
     checkConnection();
@@ -114,38 +141,35 @@ class MapdConnector {
   Future<List<Map<String, dynamic>>> query(String query,
       {bool columnFormat: true,
       int limit: -1,
-        int offset: -1,
+      int offset: -1,
       bool eliminateNullRows: false,
       bool logTiming: false}) async {
     Stopwatch stopwatch;
     if (logTiming) {
-    stopwatch = new Stopwatch();
+      stopwatch = new Stopwatch();
       stopwatch.start();
     }
     checkConnection();
 //    try {
-      var data = await client.sql_execute(
-          session, query, columnFormat, _getNonce(), offset, limit);
+    var data = await client.sql_execute(
+        session, query, columnFormat, _getNonce(), offset, limit);
 
-
-      List<Map<String, dynamic>> result;
-      if (columnFormat) {
-        result = processColumnarResults(data, eliminateNullRows);
-      } else {
-        result = processRowResults(data, eliminateNullRows);
-      }
-      if (logTiming) {
-        stopwatch.stop();
-        var duration2 = stopwatch.elapsed;
-        print(
-            '----------------------------------\n'
-                '${result
+    List<Map<String, dynamic>> result;
+    if (columnFormat) {
+      result = processColumnarResults(data, eliminateNullRows);
+    } else {
+      result = processRowResults(data, eliminateNullRows);
+    }
+    if (logTiming) {
+      stopwatch.stop();
+      var duration2 = stopwatch.elapsed;
+      print('----------------------------------\n'
+          '${result
                 .length} rows returned. Browser query time: ${duration2.inMilliseconds}, MapD execution_time_ms: ${data
                 .execution_time_ms}, MapD total_time_ms: ${data
                 .total_time_ms}\n--------\n$query');
-        stopwatch.reset();
-      }
-      return result;
+      stopwatch.reset();
+    }
+    return result;
   }
 }
-
